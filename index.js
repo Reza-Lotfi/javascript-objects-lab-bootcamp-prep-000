0fedfabb05ba0116bb29cc5cb3433720d74ba908
Reza Lotfi <rezaiscool7@aol.com>
2018-08-09 01:01:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-function deleteFromObjectByKey(Object, key){
+function deleteFromObjectByKey(object, key){
   var newObj = Object.assign({}, Object)
   delete newObj.key
   return newObj
